d0d1ca90a9695a6c7c896a06d0fe08c8ef767b19
diksh <dikshitaggarwal007@gmail.com>
2026-09-09 17:11:41 +0530

    V0.0.4.8

--- site/assets/auth.js
+++ site/assets/auth.js
@@ -1,6 +1,6 @@
 import { authApi } from './auth-api.js';
 import { requireSupabase, supabase } from './supabase-client.js';
-import { mountLayout, showToast } from './layout.js?v=3.9.0';
+import { mountLayout, showToast } from './layout.js?v=4.1.0';
 
 await mountLayout('');
 
